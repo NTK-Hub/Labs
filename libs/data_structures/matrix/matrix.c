a9161f42f537e11b2c25063d8a8998db370a85dc
NTK-Hub <ignatiev_arol@edu.bstu.ru>
2023-03-30 13:48:57 +0300

    add matrix

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -1,20 +1,288 @@
 //
 // Created by NTK on 19.02.2023.
 //
-#include "matrix.h"
+
 #include <malloc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#include "matrix.h"
+#include "../../algorithms/algorithm.h"
 
 matrix getMemMatrix(int nRows, int nCols) {
-    int **values = (int **) malloc(sizeof(int *) * nRows);
-    for (int i = 0; i < nRows; i++)
+    int **values = (int **) malloc(nRows * sizeof(int *));
+
+    if (values == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+
+    for (int i = 0; i < nRows; i++) {
         values[i] = (int *) malloc(sizeof(int) * nCols);
+        if (values[i] == NULL) {
+            fprintf(stderr, "bad alloc");
+            exit(1);
+        }
+    }
     return (matrix) {values, nRows, nCols};
 }
 
 matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols) {
-    matrix *ms = (matrix *) malloc(sizeof(matrix) * nMatrices);
+    matrix *ms = (matrix *) malloc(nMatrices * sizeof(matrix));
+    if (ms == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+
     for (int i = 0; i < nMatrices; i++)
         ms[i] = getMemMatrix(nRows, nCols);
+
     return ms;
 }
 
+void freeMemMatrix(matrix *m) {
+    for (int i = 0; i < m->nRows; i++)
+        free(m->values[i]);
+    free(m->values);
+    m->values = NULL;
+    m->nRows = 0;
+    m->nCols = 0;
+}
+
+void freeMemMatrices(matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++)
+        freeMemMatrix(&ms[i]);
+    free(ms);
+}
+
+void inputMatrix(matrix m) {
+    for (int rIndex = 0; rIndex < m.nRows; rIndex++) {
+        for (int cIndex = 0; cIndex < m.nCols; cIndex++) {
+            scanf("%d", &m.values[rIndex][cIndex]);
+        }
+    }
+}
+
+void inputMatrices(matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++) {
+        inputMatrix(ms[i]);
+    }
+}
+
+void outputMatrix(matrix m) {
+    for (int rIndex = 0; rIndex < m.nRows; rIndex++) {
+        for (int cIndex = 0; cIndex < m.nCols; cIndex++) {
+            printf("%d ", m.values[rIndex][cIndex]);
+        }
+        printf("\n");
+    }
+}
+
+void outputMatrices(matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++) {
+        outputMatrix(ms[i]);
+    }
+}
+
+void swapRows(matrix m, int i1, int i2) {
+    assert(i1 < m.nRows && i2 < m.nRows);
+
+    universalSwap(&m.values[i1], &m.values[i2], sizeof(int *));
+}
+
+void swapColumns(matrix m, int j1, int j2) {
+    assert(j1 < m.nCols && j2 < m.nCols);
+
+    for (int rIndex = 0; rIndex < m.nRows; rIndex++) {
+        universalSwap(&m.values[rIndex][j1], &m.values[rIndex][j2], sizeof(int));
+    }
+}
+
+void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int *, int)) {
+    int *rows = (int *) malloc(sizeof(int) * m.nRows);
+
+    for (int rIndex = 0; rIndex < m.nRows; ++rIndex)
+        rows[rIndex] = criteria(m.values[rIndex], m.nCols);
+
+    for (int rIndex = 1; rIndex < m.nRows; ++rIndex) {
+        int curIndex = rIndex;
+        while (curIndex > 0 && rows[curIndex] < rows[curIndex - 1]) {
+            swapRows(m, curIndex, curIndex - 1);
+            universalSwap(&rows[curIndex], &rows[curIndex - 1], sizeof(int));
+            curIndex--;
+        }
+    }
+}
+
+void insertionSortRowsMatrixByRowCriteriaFloat(matrix m, float (*criteria)(int *, int)) {
+    float *rows = (float *) malloc(sizeof(float ) * m.nRows);
+
+    for (int rIndex = 0; rIndex < m.nRows; ++rIndex)
+        rows[rIndex] = criteria(m.values[rIndex], m.nCols);
+
+    for (int rIndex = 1; rIndex < m.nRows; ++rIndex) {
+        int curIndex = rIndex;
+        while (curIndex > 0 && rows[curIndex] < rows[curIndex - 1]) {
+            swapRows(m, curIndex, curIndex - 1);
+            universalSwap(&rows[curIndex], &rows[curIndex - 1], sizeof(int));
+            curIndex--;
+        }
+    }
+}
+
+void insertionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int *, int)) {
+    int *cols = (int *) malloc(sizeof(int) * m.nCols);
+    int *curCols = malloc(sizeof(int) * m.nRows);
+
+    for (int cIndex = 0; cIndex < m.nCols; ++cIndex) {
+        for (int rIndex = 0; rIndex < m.nRows; ++rIndex) {
+            curCols[rIndex] = m.values[rIndex][cIndex];
+        }
+        cols[cIndex] = criteria(curCols, m.nRows);
+    }
+
+    for (int cIndex = 1; cIndex < m.nCols; ++cIndex) {
+        int curIndex = cIndex;
+        while (curIndex > 0 && cols[curIndex] < cols[curIndex - 1]) {
+            swapColumns(m, curIndex, curIndex - 1);
+            universalSwap(&cols[curIndex], &cols[curIndex - 1], sizeof(int));
+            curIndex--;
+        }
+    }
+}
+
+bool isSquareMatrix(matrix m) {
+    return m.nRows == m.nCols;
+}
+
+bool areTwoMatricesEqual(matrix m1, matrix m2) {
+    if (m1.nRows != m2.nRows || m1.nCols != m2.nCols)
+        return false;
+
+    for (int rIndex = 0; rIndex < m1.nRows; rIndex++) {
+        for (int cIndex = 0; cIndex < m1.nCols; ++cIndex) {
+            if (m1.values[rIndex][cIndex] != m2.values[rIndex][cIndex])
+                return false;
+        }
+    }
+    return true;
+}
+
+bool isEMatrix(matrix m) {
+    if (!isSquareMatrix(m))
+        return false;
+
+    for (int rIndex = 0; rIndex < m.nRows; rIndex++) {
+        for (int cIndex = 0; cIndex < m.nCols; ++cIndex) {
+            if (rIndex == cIndex && m.values[rIndex][cIndex] != 1
+                || rIndex != cIndex && m.values[rIndex][cIndex] != 0)
+                return false;
+        }
+    }
+    return true;
+}
+
+bool isSymmetricMatrix(matrix m) {
+    if (!isSquareMatrix(m))
+        return false;
+
+    for (int rIndex = 0; rIndex < m.nRows; rIndex++) {
+        for (int cIndex = 0; cIndex < m.nCols; cIndex++) {
+            int rowsIndex = cIndex;
+            int colsIndex = rIndex;
+
+            if (m.values[rIndex][cIndex] != m.values[rowsIndex][colsIndex])
+                return false;
+        }
+    }
+    return true;
+}
+
+void transposeSquareMatrix(matrix m) {
+    assert(isSquareMatrix(m));
+
+    for (int rIndex = 0; rIndex < m.nRows; ++rIndex) {
+        for (int cIndex = 0; cIndex < rIndex; ++cIndex) {
+            if (rIndex != cIndex)
+                universalSwap(&m.values[rIndex][cIndex], &m.values[cIndex][rIndex], sizeof(int));
+        }
+    }
+}
+
+void transposeMatrix(matrix *m) {
+    matrix newMatrix = getMemMatrix(m->nCols, m->nRows);
+
+    for (int i = 0; i < m->nRows; i++)
+        for (int j = 0; j < m->nCols; j++)
+            newMatrix.values[j][i] = m->values[i][j];
+
+    freeMemMatrix(m);
+    *m = newMatrix;
+}
+
+position getMinValuePos(matrix m) {
+    position minValuePos = {0, 0};
+
+    for (int rIndex = 0; rIndex < m.nRows; rIndex++) {
+        for (int cIndex = 0; cIndex < m.nCols; cIndex++) {
+            position currentPos = {rIndex, cIndex};
+            if (m.values[currentPos.rowIndex][currentPos.colIndex] <
+                m.values[minValuePos.rowIndex][minValuePos.colIndex])
+                minValuePos = currentPos;
+        }
+    }
+    return minValuePos;
+}
+
+position getMaxValuePos(matrix m) {
+    position maxValuePos = {0, 0};
+
+    for (int rIndex = 0; rIndex < m.nRows; rIndex++) {
+        for (int cIndex = 0; cIndex < m.nCols; cIndex++) {
+            position currentPos = {rIndex, cIndex};
+            if (m.values[currentPos.rowIndex][currentPos.colIndex] >
+                m.values[maxValuePos.rowIndex][maxValuePos.colIndex])
+                maxValuePos = currentPos;
+        }
+    }
+    return maxValuePos;
+}
+
+matrix createMatrixFromArray(const int *a, int nRows, int nCols) {
+    matrix m = getMemMatrix(nRows, nCols);
+
+    int k = 0;
+    for (int rIndex = 0; rIndex < nRows; rIndex++)
+        for (int cIndex = 0; cIndex < nCols; cIndex++)
+            m.values[rIndex][cIndex] = a[k++];
+
+    return m;
+}
+
+matrix *createArrayOfMatrixFromArray(const int *values, int nMatrices, int nRows, int nCols) {
+    matrix *ms = getMemArrayOfMatrices(nMatrices, nRows, nCols);
+
+    int l = 0;
+    for (int k = 0; k < nMatrices; k++)
+        for (int rIndex = 0; rIndex < nRows; rIndex++)
+            for (int cIndex = 0; cIndex < nCols; cIndex++)
+                ms[k].values[rIndex][cIndex] = values[l++];
+
+    return ms;
+}
+
+matrix mulMatrices(matrix m1, matrix m2) {
+    matrix mulMatrix = getMemMatrix(m1.nRows, m2.nCols);
+
+    for (int i = 0; i < m1.nRows; ++i) {
+        for (int j = 0; j < m2.nCols; ++j) {
+            mulMatrix.values[i][j] = 0;
+            for (int k = 0; k < m1.nCols; ++k) {
+                mulMatrix.values[i][j] += m1.values[i][k] * m2.values[k][j];
+            }
+        }
+    }
+    return mulMatrix;
+}
--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -4,33 +4,99 @@
 
 #ifndef LABS_MATRIX_H
 
+#include <stdbool.h>
+
 typedef struct matrix {
-    int **values; // элементы матрицы
-    int nRows; // количество рядов
-    int nCols; // количество столбцов
+    int **values; //элементы матрицы
+    int nRows; //количество рядов
+    int nCols; //количество столбцов
 } matrix;
+
 typedef struct position {
     int rowIndex;
     int colIndex;
 } position;
 
-matrix getMemMatrix(int nRows, int nCols); // размещает в динамической памяти матрицу размером nRows на nCols. Возвращает матрицу.
+//размещает в динамической памяти матрицу размером nRows на nCols
+//возвращает матрицу
+matrix getMemMatrix(int nRows, int nCols);
 
-matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols); // размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols. Возвращает указатель на нулевую матрицу.
+//размещает в динамической памяти массив из nMatrices матриц размероми nRows на nCols
+//возвращает указатель на нулевую матрицу
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
 
-void freeMemMatrix(matrix *m); // освобождает память, выделенную под хранение матрицы m.
+//освобождает память, выделенную под матрицу m
+void freeMemMatrix(matrix *m);
 
-void freeMemMatrices(matrix *ms, int nMatrices); // освобождает память, выделенную под хранение массива ms из nMatrices матриц.
+//освобождает память, выделенную под хранение массива ms из nMatrices матриц
+void freeMemMatrices(matrix *ms, int nMatrices);
 
-void inputMatrix(matrix *m); // ввод матрицы m.
+//ввод матрицы m
+void inputMatrix(matrix m);
 
-void inputMatrices(matrix *ms, int nMatrices); // ввод массива из nMatrices матриц, хранящейся по адресу ms
+//ввод массива ms из nMatrices матриц
+void inputMatrices(matrix *ms, int nMatrices);
 
-void outputMatrix(matrix m); //  вывод матрицы m.
+//вывод матрицы m
+void outputMatrix(matrix m);
 
-void outputMatrices(matrix *ms, int nMatrices); // вывод массива из nMatrices матриц, хранящейся по адресу ms.
+//вывод массива ms из nMatrices матриц
+void outputMatrices(matrix *ms, int nMatrices);
 
-void swapRows(matrix m, int i1, int i2); // обмен строк с порядковыми номерами i1 и i2 в матрице m. Помните, что для этого достаточно обменять указатели соответствующих строк:
+//обмен строк с порядковыми номерами i1 и i2 в матрице m
+void swapRows(matrix m, int i1, int i2);
+
+//обмен колонок с порядковыми номерами j1 и j2 в матрице m
+void swapColumns(matrix m, int j1, int j2);
+
+//выполняет сортировку вставками строк матрицы m по неубыванию значения функции criteria
+//применяемой для строк
+void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int *, int));
+
+
+void insertionSortRowsMatrixByRowCriteriaFloat(matrix m, float (*criteria)(int *, int));
+
+//выполняет сортировку вставками столбцов матрицы m по неубыванию значения
+//функции criteria применяемой для столбцов
+void insertionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int *, int));
+
+//возвращает значение ’истина’, если матрица m является квадратной, ложь – в противном случае
+bool isSquareMatrix(matrix m);
+
+//возвращает значение ’истина’, если матрицы m1 и m2 равны, ложь – в противном случае.
+bool areTwoMatricesEqual(matrix m1, matrix m2);
+
+//возвращает значение ’истина’, если матрица
+//m является единичной, ложь – в противном случае
+bool isEMatrix(matrix m);
+
+//возвращает значение ’истина’, если
+//матрица m является симметричной, ложь – в противном случае
+bool isSymmetricMatrix(matrix m);
+
+//транспонирует квадратную матрицу m
+void transposeSquareMatrix(matrix m);
+
+//возвращает позицию максимального элемента матрицы m
+void transposeMatrix(matrix *m);
+
+//возвращает позицию минимального элемента матрицы m
+position getMinValuePos(matrix m);
+
+//возвращает позицию максимального элемента матрицы m
+position getMaxValuePos(matrix m);
+
+//возвращает матрицу, размера nRows на nCols, построенного
+//из элементов массива a, размещенную в динамической памяти
+matrix createMatrixFromArray(const int *a, int nRows, int nCols);
+
+//возвращает указатель на нулевую матрицу массива из nMatrices матриц, размещенных
+//в динамической памяти, построенных из элементов массива a
+matrix *createArrayOfMatrixFromArray(const int *values,
+                                     int nMatrices, int nRows, int nCols);
+
+//возвращает произведение двух матриц m1 и m2
+matrix mulMatrices(matrix m1, matrix m2);
 
 #define LABS_MATRIX_H
 
